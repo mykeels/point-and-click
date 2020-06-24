136497d87a2fa23d0ba3396f155c5c29eea159b8
mykeels <mykehell123@gmail.com>
2020-06-24 18:41:37 +0100

    feat: consume graphql api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import '@aws-amplify/ui/dist/style.css';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import { withAuthenticator } from 'aws-amplify-react';
-import Amplify from "aws-amplify";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
+import { listTodos } from "./graphql/queries";
 
 Amplify.configure(awsconfig);
 
 function App() {
+  const [points, setPoints] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await API.graphql(graphqlOperation(listTodos));
+      setPoints(res?.data?.listTodos?.items);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          points.map(point => <p key={point.id}>
+            X: <span>{point.x}</span>, Y: <span>{point.y}</span>
+          </p>)
+        }
       </header>
     </div>
   );
